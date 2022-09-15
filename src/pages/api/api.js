fae34fbe7fdfc0bd43c1399b073bc122de632cd1
Leeh003 <ltc.rms21@gmail.com>
2022-09-15 12:12:26 -0300

    Add api para tela de investimento

--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -13,7 +13,61 @@ const Currencies = async (currencie) => {
     var json = JSON.parse(data);
     var moeda = json.results.currencies;
 
-    return moeda; 
+    return moeda;
 };
 
-export default Currencies;
+const Bitcoin = async () => {
+    let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+    }
+
+    let response = await fetch("https://api.hgbrasil.com/finance/quotations?format=json-cors&key=f107716c&fields=bitcoin", {
+        method: "GET",
+        headers: headersList
+    });
+
+    let data = await response.text();
+    var json = JSON.parse(data);
+    var bitcoin = json.results.bitcoin;
+
+    return bitcoin;
+};
+
+const Stocks = async () => {
+    let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+    }
+
+    let response = await fetch("https://api.hgbrasil.com/finance/quotations?format=json-cors&key=f107716c&fields=stocks", {
+        method: "GET",
+        headers: headersList
+    });
+
+    let data = await response.text();
+    var json = JSON.parse(data);
+    var stocks = json.results.stocks;
+
+    return stocks;
+};
+
+const Taxes = async () => {
+    let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)"
+    }
+
+    let response = await fetch("https://api.hgbrasil.com/finance/taxes?format=json-cors&key=f107716c", {
+        method: "GET",
+        headers: headersList
+    });
+
+    let data = await response.text();
+    var json = JSON.parse(data);
+    var taxes = json.results;
+
+    return taxes;
+};
+
+export { Currencies, Bitcoin, Stocks, Taxes };
